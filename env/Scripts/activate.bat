648310650a5dd5c3114d754b82fe2e67092dfda3
jeMartine <jorgeesteban464@gmail.com>
2023-11-02 21:48:14 -0500

    prueba funcioamiento repo

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\user\Desktop\ProyectoIFW\env
+set VIRTUAL_ENV=C:\Users\esteb\Desktop\FIS_Queni\env
 
 if not defined PROMPT set PROMPT=$P$G
 
